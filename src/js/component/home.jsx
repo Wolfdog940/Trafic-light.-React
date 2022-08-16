ea4a1ef5d2f2d9812df46e3abd793f1f807d7209
Oscar Urrutia <oscarurrutia940@gmail.com>
2022-08-16 17:35:18 +0000

    estilos ok-faltan extras

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,19 +4,27 @@ const Home = () => {
   const [color, setColor] = useState("");
 
   return (
-    <div className="semaforo">
-      <div
-        onClick={() => setColor("red")}
-        className={"light red " + (color === "red" ? "glowred" : "")}
-      ></div>
+    <div className="trafic">
+      <div className="semaforo">
+        <div
+          onClick={() => setColor("red")}
+          className={"light red " + (color === "red" ? "glowred" : "")}
+        ></div>
 
-      <div
-        onClick={() => setColor("yellow")}
-        className={"light yellow " + (color === "yellow" ? "glowyellow" : "")}></div>
-      <div
-        onClick={() => setColor("green")}
-        className={"light green " + (color === "green" ? "glowgreen" : "")}
-      ></div>
+        <div
+          onClick={() => setColor("yellow")}
+          className={"light yellow " + (color === "yellow" ? "glowyellow" : "")}
+        ></div>
+        <div
+          onClick={() => setColor("green")}
+          className={"light green " + (color === "green" ? "glowgreen" : "")}
+        ></div>
+		
+      </div>
+
+      <div className="palo fixed-top"></div>
+        
+      
     </div>
   );
 };
