2acf5e0b59c50963b9b43b3b6822b09af964d930
Oscar Urrutia <oscarurrutia940@gmail.com>
2022-08-16 16:22:31 +0000

    luces ok

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,21 +1,21 @@
 import React, { useState } from "react";
 
 const Home = () => {
-  const [color, Setcolor] = useState("");
+  const [color, setColor] = useState("");
 
   return (
     <div className="semaforo">
       <div
-        onClick={() => Setcolor("red")}
+        onClick={() => setColor("red")}
         className={"light red " + (color === "red" ? "glowred" : "")}
       ></div>
 
       <div
-        onClick={() => Setcolor("yellow")}
-        className={"light yellow" + (color === "yellow" ? "glowyellow" : "")}></div>
+        onClick={() => setColor("yellow")}
+        className={"light yellow " + (color === "yellow" ? "glowyellow" : "")}></div>
       <div
-        onClick={() => Setcolor("green")}
-        className={"light green" + (color === "green" ? "glowgreen" : "")}
+        onClick={() => setColor("green")}
+        className={"light green " + (color === "green" ? "glowgreen" : "")}
       ></div>
     </div>
   );
